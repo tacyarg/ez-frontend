1dbd0dad49a2a60d4e8f1b4db1130b261c454e66
Jacob Dent <jdent@tacyarg.com>
2019-11-24 12:42:04 -0400

    missing files

---
+++ src/pages/DepositGateway.js
@@ -0,0 +1,8 @@
+import React, { useState, useEffect } from 'react'
+import { Button, Text, Box, Flex } from '../primitives'
+
+export default p => {
+  return <>
+    <Button>TACOS</Button>
+  </>
+}
